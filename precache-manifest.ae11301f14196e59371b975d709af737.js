5f0dbe6014fde4d4e720c892fe2bc67973cc4480
rhbc73 <rhbc73@gmail.com>
2018-10-15 00:06:49 +1100

    Updates

---
+++ precache-manifest.ae11301f14196e59371b975d709af737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ca634c39cb232d5eac4",
+    "url": "/card_front/static/js/runtime~main.8ca634c3.js"
+  },
+  {
+    "revision": "867670a7867dfec5ab94",
+    "url": "/card_front/static/js/main.867670a7.chunk.js"
+  },
+  {
+    "revision": "8dee197ccb5f45018480",
+    "url": "/card_front/static/js/1.8dee197c.chunk.js"
+  },
+  {
+    "revision": "867670a7867dfec5ab94",
+    "url": "/card_front/static/css/main.5c53d580.chunk.css"
+  },
+  {
+    "revision": "8dee197ccb5f45018480",
+    "url": "/card_front/static/css/1.b67ba98a.chunk.css"
+  },
+  {
+    "revision": "586f02d8a4278bf69793b993ac07b82a",
+    "url": "/card_front/index.html"
+  }
+];
